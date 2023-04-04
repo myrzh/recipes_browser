638974d564ca67f853b398e82b38372891d77dc7
enoughtless <enoughtless@gmail.com>
2023-04-04 22:23:37 +0500

    .

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 from data import db_session
-from routes import blueprint
+# from routes import blueprint
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("flask_app/db/db.sqlite")
-    app.register_blueprint
+    # app.register_blueprint
     app.run()
 
 
---
+++ flask_app/data/recipes.py
@@ -0,0 +1,26 @@
+import sqlalchemy
+from sqlalchemy import orm
+from datetime import datetime
+
+from .db_session import SqlAlchemyBase
+
+
+class Recipes(SqlAlchemyBase):
+    __tablename__ = 'recipes'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, 
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    text_content = sqlalchemy.Column(sqlalchemy.VARCHAR, nullable=True)
+
+    photos_paths = sqlalchemy.Column(sqlalchemy.VARCHAR, nullable=True)
+
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, 
+                                     default=datetime.now)
+
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+
+    list_of_products = sqlalchemy.Column(sqlalchemy.VARCHAR, nullable=True)
+
+    user = orm.relationship('User')
